33ba838a6e4144d0163b6e106d8f1a52b0ec51c6
Janne Savolainen <janne.savolainen@live.fi>
2021-11-30 13:35:48 +0200

    Add missing export

--- packages/injectable-react/src/index.js
+++ packages/injectable-react/src/index.js
@@ -1,3 +1,4 @@
 import Inject, { DiContextProvider } from './Inject/Inject';
+import getInjectedComponent from './getInjectedComponent/getInjectedComponent';
 
-export { Inject, DiContextProvider };
+export { Inject, getInjectedComponent, DiContextProvider };
