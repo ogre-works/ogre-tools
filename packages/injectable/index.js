903bc2328d0339cae405c8092fca907095848b86
Iku-turso <mikko.aspiala@gmail.com>
2022-02-18 16:29:45 +0200

    chore: Expose dependency graphing for public use

--- packages/injectable/index.js
+++ packages/injectable/index.js
@@ -2,5 +2,16 @@ import getInjectionToken from './src/getInjectionToken/getInjectionToken';
 import getInjectable from './src/getInjectable/getInjectable';
 import lifecycleEnum from './src/dependency-injection-container/lifecycleEnum';
 import createContainer from './src/dependency-injection-container/createContainer';
+import {
+  registerDependencyGraphing,
+  plantUmlDependencyGraphInjectable,
+} from './src/dependency-injection-container/extensions/dependency-graphing/dependency-graphing';
 
-export { getInjectionToken, getInjectable, lifecycleEnum, createContainer };
+export {
+  getInjectionToken,
+  getInjectable,
+  lifecycleEnum,
+  createContainer,
+  registerDependencyGraphing,
+  plantUmlDependencyGraphInjectable,
+};
