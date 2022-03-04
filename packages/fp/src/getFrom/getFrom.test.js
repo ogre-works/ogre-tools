b9b2d7ba93776ab41d6bc55e028bdd578f5d54bc
Janne Savolainen <janne.savolainen@live.fi>
2022-03-04 09:11:12 +0200

    chore: Fix typo

--- packages/fp/src/getFrom/getFrom.test.js
+++ packages/fp/src/getFrom/getFrom.test.js
@@ -1,20 +1,20 @@
-import getSafeFrom from './getFrom';
+import getFrom from './getFrom';
 
-describe('getSafeFrom', () => {
+describe('getFrom', () => {
   it('when getting existing value, returns value', () => {
-    const actual = getSafeFrom({ someProperty: 'some-value' })('someProperty');
+    const actual = getFrom({ someProperty: 'some-value' })('someProperty');
 
     expect(actual).toBe('some-value');
   });
 
   it('when getting non existing value, return undefined', () => {
-    const actual = getSafeFrom({})('someNonExistingProperty');
+    const actual = getFrom({})('someNonExistingProperty');
 
     expect(actual).toBeUndefined();
   });
 
   it('when getting existing nested value, returns value', () => {
-    const actual = getSafeFrom({ someRoot: { someProperty: 'some-value' } })(
+    const actual = getFrom({ someRoot: { someProperty: 'some-value' } })(
       'someRoot.someProperty',
     );
 
