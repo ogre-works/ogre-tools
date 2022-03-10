55fd4d20241bd044be5668d80df4fdc493311da4
Iku-turso <mikko.aspiala@gmail.com>
2022-03-10 13:07:51 +0200

    fix: Add missing export for error monitoring in JS

--- packages/injectable/index.js
+++ packages/injectable/index.js
@@ -2,10 +2,16 @@ import getInjectionToken from './src/getInjectionToken/getInjectionToken';
 import getInjectable from './src/getInjectable/getInjectable';
 import lifecycleEnum from './src/dependency-injection-container/lifecycleEnum';
 import createContainer from './src/dependency-injection-container/createContainer';
+
 import {
   registerDependencyGraphing,
   plantUmlDependencyGraphInjectable,
 } from './src/dependency-injection-container/extensions/dependency-graphing/dependency-graphing';
+
+import {
+  registerErrorMonitoring,
+  errorMonitorInjectionToken,
+} from '@ogre-tools/injectable';
 
 export {
   getInjectionToken,
@@ -14,4 +20,6 @@ export {
   createContainer,
   registerDependencyGraphing,
   plantUmlDependencyGraphInjectable,
+  registerErrorMonitoring,
+  errorMonitorInjectionToken,
 };
