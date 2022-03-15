6330494fe11a49ffbf582a9a5372d0c9166c5c13
Sebastian Malton <sebastian@malton.name>
2022-03-15 12:02:31 -0400

    fix: Make the type of lifecycleEnum.singleton actually like ILifecycle
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- packages/injectable/ogre-tools-injectable.d.ts
+++ packages/injectable/ogre-tools-injectable.d.ts
@@ -155,7 +155,9 @@ declare module '@ogre-tools/injectable' {
   const nonStoredInstanceKey: unique symbol;
 
   export const lifecycleEnum: {
-    singleton: (di: DiContainer, param: void) => typeof storedInstanceKey;
+    singleton: {
+      getInstanceKey: (di: DiContainer, param: void) => typeof storedInstanceKey;
+    };
 
     keyedSingleton<InstantiationParam>(
       options: ILifecycle<InstantiationParam>,
