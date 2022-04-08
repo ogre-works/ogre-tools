19b2d45eccd34698f6709b1f3eaf2cc849475a2c
Janne Savolainen <janne.savolainen@live.fi>
2022-04-08 11:02:22 +0300

    feat: Introduce automated testing for typings

--- packages/injectable/ogre-tools-injectable.d.ts
+++ packages/injectable/ogre-tools-injectable.d.ts
@@ -1,164 +1,164 @@
 /// <reference types="jest" />
-declare module '@ogre-tools/injectable' {
-  export interface DiContainer extends DiContainerForInjection {
-    purge: (injectableKey: Injectable<any, any, any>) => void;
+export interface DiContainer extends DiContainerForInjection {
+  purge: (injectableKey: Injectable<any, any, any>) => void;
 
-    permitSideEffects: (injectableKey: Injectable<any, any, any>) => void;
+  permitSideEffects: (injectableKey: Injectable<any, any, any>) => void;
 
-    override<
-      InjectionInstance extends InjectionTokenInstance,
-      InjectionTokenInstance,
-      InstantiationParam,
-    >(
-      injectable: Injectable<
-        InjectionInstance,
-        InjectionTokenInstance,
-        InstantiationParam
-      >,
-      instantiateStub: Instantiate<InjectionInstance, InstantiationParam>,
-    ): void;
-
-    register<
-      InjectionInstance extends InjectionTokenInstance,
-      InjectionTokenInstance,
-      InstantiationParam,
-    >(
-      ...injectables: Injectable<
-        InjectionInstance,
-        InjectionTokenInstance,
-        InstantiationParam
-      >[]
-    ): void;
-    preventSideEffects: () => void;
-  }
-
-  export type Instantiate<InjectionInstance, InstantiationParam> = {
-    (
-      di: DiContainerForInjection,
-      param: InstantiationParam extends void ? void : InstantiationParam,
-    ): InjectionInstance;
-  };
-
-  export interface InjectionToken<InjectionInstance, InstantiationParam> {
-    template: InjectionInstance;
-    instantiationParameter: InstantiationParam;
-    key: Symbol;
-  }
-
-  export interface Injectable<
+  override<
     InjectionInstance extends InjectionTokenInstance,
     InjectionTokenInstance,
     InstantiationParam,
-  > {
-    id: string;
-    causesSideEffects?: boolean;
-    injectionToken?: InjectionToken<InjectionTokenInstance, InstantiationParam>;
-    instantiate: Instantiate<InjectionInstance, InstantiationParam>;
-    lifecycle: ILifecycle<InstantiationParam>;
-  }
-
-  type InjectableLifecycle<InstantiationParam> = InstantiationParam extends void
-    ? {
-        lifecycle?: ILifecycle<void>;
-      }
-    : {
-        lifecycle: ILifecycle<InstantiationParam>;
-      };
-
-  type GetInjectableOptions<
-    InjectionInstance extends InjectionTokenInstance,
-    InjectionTokenInstance,
-    InstantiationParam,
-  > = InjectableLifecycle<InstantiationParam> &
-    Omit<
-      Injectable<InjectionInstance, InjectionTokenInstance, InstantiationParam>,
-      'lifecycle'
-    >;
-
-  export function getInjectable<
-    InjectionInstance extends InjectionTokenInstance,
-    InjectionTokenInstance,
-    InstantiationParam = void,
   >(
-    options: GetInjectableOptions<
+    injectable: Injectable<
       InjectionInstance,
       InjectionTokenInstance,
       InstantiationParam
     >,
-  ): Injectable<InjectionInstance, InjectionTokenInstance, InstantiationParam>;
+    instantiateStub: Instantiate<InjectionInstance, InstantiationParam>,
+  ): void;
 
-  export function getInjectionToken<
-    InjectionInstance,
-    InstantiationParam = void,
-  >({ id: string }): InjectionToken<InjectionInstance, InstantiationParam>;
-
-  interface Inject {
-    <InjectionInstance>(
-      key:
-        | Injectable<InjectionInstance, unknown, void>
-        | InjectionToken<InjectionInstance, void>,
-    ): InjectionInstance;
-
-    <InjectionInstance, InstantiationParam>(
-      key:
-        | Injectable<InjectionInstance, unknown, InstantiationParam>
-        | InjectionToken<InjectionInstance, InstantiationParam>,
-      param: InstantiationParam,
-    ): InjectionInstance;
-  }
-
-  interface InjectMany {
-    <InjectionInstance>(
-      key:
-        | Injectable<InjectionInstance, unknown, void>
-        | InjectionToken<InjectionInstance, void>,
-    ): InjectionInstance[];
-
-    <InjectionInstance, InstantiationParam>(
-      key:
-        | Injectable<InjectionInstance, unknown, InstantiationParam>
-        | InjectionToken<InjectionInstance, InstantiationParam>,
-      param: InstantiationParam,
-    ): InjectionInstance[];
-  }
-
-  export interface DiContainerForInjection {
-    inject: Inject;
-    injectMany: InjectMany;
-
-    register<
-      InjectionInstance extends InjectionTokenInstance,
+  register<
+    InjectionInstance extends InjectionTokenInstance,
+    InjectionTokenInstance,
+    InstantiationParam,
+  >(
+    ...injectables: Injectable<
+      InjectionInstance,
       InjectionTokenInstance,
-      InstantiationParam,
-    >(
-      ...injectables: Injectable<
-        InjectionInstance,
-        InjectionTokenInstance,
-        InstantiationParam
-      >[]
-    ): void;
-  }
+      InstantiationParam
+    >[]
+  ): void;
+  preventSideEffects: () => void;
+}
 
-  export interface ILifecycle<InstantiationParam> {
-    getInstanceKey: (di: DiContainer, params: InstantiationParam) => any;
-  }
+export type Instantiate<InjectionInstance, InstantiationParam> = {
+  (
+    di: DiContainerForInjection,
+    param: InstantiationParam extends void ? void : InstantiationParam,
+  ): InjectionInstance;
+};
 
-  const storedInstanceKey: unique symbol;
-  const nonStoredInstanceKey: unique symbol;
+export interface InjectionToken<InjectionInstance, InstantiationParam> {
+  template: InjectionInstance;
+  instantiationParameter: InstantiationParam;
+  key: Symbol;
+}
 
-  export const lifecycleEnum: {
-    singleton: {
-      getInstanceKey: (di: DiContainer) => typeof storedInstanceKey;
+export interface Injectable<
+  InjectionInstance extends InjectionTokenInstance,
+  InjectionTokenInstance,
+  InstantiationParam,
+> {
+  id: string;
+  causesSideEffects?: boolean;
+  injectionToken?: InjectionToken<InjectionTokenInstance, InstantiationParam>;
+  instantiate: Instantiate<InjectionInstance, InstantiationParam>;
+  lifecycle: ILifecycle<InstantiationParam>;
+}
+
+type InjectableLifecycle<InstantiationParam> = InstantiationParam extends void
+  ? {
+      lifecycle?: ILifecycle<void>;
+    }
+  : {
+      lifecycle: ILifecycle<InstantiationParam>;
     };
 
-    keyedSingleton<InstantiationParam>(
-      options: ILifecycle<InstantiationParam>,
-    ): typeof options;
+type GetInjectableOptions<
+  InjectionInstance extends InjectionTokenInstance,
+  InjectionTokenInstance,
+  InstantiationParam,
+> = InjectableLifecycle<InstantiationParam> &
+  Omit<
+    Injectable<InjectionInstance, InjectionTokenInstance, InstantiationParam>,
+    'lifecycle'
+  >;
 
-    transient: {
-      getInstanceKey: (di: DiContainer) => typeof nonStoredInstanceKey;
-    };
+export function getInjectable<
+  InjectionInstance extends InjectionTokenInstance,
+  InjectionTokenInstance,
+  InstantiationParam = void,
+>(
+  options: GetInjectableOptions<
+    InjectionInstance,
+    InjectionTokenInstance,
+    InstantiationParam
+  >,
+): Injectable<InjectionInstance, InjectionTokenInstance, InstantiationParam>;
+
+export function getInjectionToken<
+  InjectionInstance,
+  InstantiationParam = void,
+>(options: {
+  id: string;
+}): InjectionToken<InjectionInstance, InstantiationParam>;
+
+interface Inject {
+  <InjectionInstance>(
+    key:
+      | Injectable<InjectionInstance, unknown, void>
+      | InjectionToken<InjectionInstance, void>,
+  ): InjectionInstance;
+
+  <InjectionInstance, InstantiationParam>(
+    key:
+      | Injectable<InjectionInstance, unknown, InstantiationParam>
+      | InjectionToken<InjectionInstance, InstantiationParam>,
+    param: InstantiationParam,
+  ): InjectionInstance;
+}
+
+interface InjectMany {
+  <InjectionInstance>(
+    key:
+      | Injectable<InjectionInstance, unknown, void>
+      | InjectionToken<InjectionInstance, void>,
+  ): InjectionInstance[];
+
+  <InjectionInstance, InstantiationParam>(
+    key:
+      | Injectable<InjectionInstance, unknown, InstantiationParam>
+      | InjectionToken<InjectionInstance, InstantiationParam>,
+    param: InstantiationParam,
+  ): InjectionInstance[];
+}
+
+export interface DiContainerForInjection {
+  inject: Inject;
+  injectMany: InjectMany;
+
+  register<
+    InjectionInstance extends InjectionTokenInstance,
+    InjectionTokenInstance,
+    InstantiationParam,
+  >(
+    ...injectables: Injectable<
+      InjectionInstance,
+      InjectionTokenInstance,
+      InstantiationParam
+    >[]
+  ): void;
+}
+
+export interface ILifecycle<InstantiationParam> {
+  getInstanceKey: (di: DiContainer, params: InstantiationParam) => any;
+}
+
+declare const storedInstanceKey: unique symbol;
+declare const nonStoredInstanceKey: unique symbol;
+
+export const lifecycleEnum: {
+  singleton: {
+    getInstanceKey: (di: DiContainer) => typeof storedInstanceKey;
   };
 
-  export function createContainer(): DiContainer;
-}
+  keyedSingleton<InstantiationParam>(
+    options: ILifecycle<InstantiationParam>,
+  ): typeof options;
+
+  transient: {
+    getInstanceKey: (di: DiContainer) => typeof nonStoredInstanceKey;
+  };
+};
+
+export function createContainer(): DiContainer;
---
+++ packages/injectable/ogre-tools-injectable.test-d.ts
@@ -0,0 +1,191 @@
+import { expectError, expectNotType, expectType } from 'tsd';
+
+import {
+  createContainer,
+  DiContainer,
+  DiContainerForInjection,
+  getInjectable,
+  getInjectionToken,
+  lifecycleEnum,
+} from '.';
+
+const di = createContainer();
+
+// given injectable with unspecified type for instantiation parameter, argument typing is OK
+const someInjectableForTypingOfInstantiate = getInjectable({
+  id: 'some-injectable',
+
+  instantiate: (di, instantiationParameter) => {
+    expectType<DiContainerForInjection>(di);
+    expectType<void>(instantiationParameter);
+  },
+
+  lifecycle: lifecycleEnum.keyedSingleton({
+    getInstanceKey: (di, instantiationParameter) => {
+      expectType<DiContainer>(di);
+      expectType<void>(instantiationParameter);
+    },
+  }),
+});
+
+const someInjectableWithMatchingInstantiationParameters = getInjectable({
+  id: 'some-injectable',
+
+  instantiate: (di, instantiationParameter: string) => {
+    expectType<string>(instantiationParameter);
+  },
+
+  lifecycle: lifecycleEnum.keyedSingleton({
+    getInstanceKey: (di, instantiationParameter: string) => {
+      expectType<string>(instantiationParameter);
+    },
+  }),
+});
+
+// given injectable with mismatching types for instantiation parameter, typing is not OK
+expectError(
+  getInjectable({
+    id: 'some-injectable',
+
+    instantiate: (di, instantiationParameter: number) => {},
+
+    lifecycle: lifecycleEnum.keyedSingleton({
+      getInstanceKey: (di, instantiationParameter: string) =>
+        instantiationParameter,
+    }),
+  }),
+);
+
+const someInjectableWithoutInstantiationParameter = getInjectable({
+  id: 'some-injectable',
+  instantiate: () => 'some string',
+  lifecycle: lifecycleEnum.transient,
+});
+
+// given injectable without instantiation parameters, when injected without parameter, typing is OK
+expectType<string>(di.inject(someInjectableWithoutInstantiationParameter));
+
+// given injectable without instantiation parameters, when injected with parameter, typing is not OK
+expectError(di.inject(someInjectableWithoutInstantiationParameter, 42));
+
+const someInjectableWithInstantiationParameter = getInjectable({
+  id: 'some-other-injectable',
+  instantiate: (di, instantiationParameter: number) => instantiationParameter,
+  lifecycle: lifecycleEnum.transient,
+});
+
+// given injectable with instantiation parameters, when injected with parameter, typing is OK
+expectType<number>(di.inject(someInjectableWithInstantiationParameter, 42));
+
+// given injectable with instantiation parameters, when injected without parameter, typing is not OK
+expectError(di.inject(someInjectableWithInstantiationParameter));
+
+// given injectable with instantiation parameters, when injected with parameter of wrong type, typing is not OK
+expectError(
+  di.inject(someInjectableWithInstantiationParameter, 'some-not-number'),
+);
+
+const someInjectionToken = getInjectionToken<{
+  requiredProperty: string;
+  optionalProperty?: number;
+}>({
+  id: 'some-injection-token',
+});
+
+// given injection token, when creating implementation with wrong interface, typing is not OK
+expectError(
+  getInjectable({
+    id: 'some-injectable',
+    instantiate: () => ({}),
+    injectionToken: someInjectionToken,
+  }),
+);
+
+// given injection token, when creating implementation with incomplete interface, typing is not OK
+expectError(
+  getInjectable({
+    id: 'some-injectable',
+    instantiate: () => ({ requiredProperty: 42 }),
+    injectionToken: someInjectionToken,
+  }),
+);
+
+// given injection token, when creating implementation with mandatory but no optional properties, typing is OK
+expectNotType<any>(
+  getInjectable({
+    id: 'some-injectable',
+    instantiate: () => ({ requiredProperty: 'some string' }),
+    injectionToken: someInjectionToken,
+  }),
+);
+
+// given injection token, when creating implementation with mandatory and optional properties, typing is OK
+expectNotType<any>(
+  getInjectable({
+    id: 'some-injectable',
+
+    instantiate: () => ({
+      requiredProperty: 'some string',
+      optionalProperty: 42,
+    }),
+
+    injectionToken: someInjectionToken,
+  }),
+);
+
+const someTokenWithGeneralProperty = getInjectionToken<{
+  someGeneralProperty: string;
+}>({
+  id: 'some-token-with-general-property',
+});
+
+const someInjectableWithAlsoSpecificProperty = getInjectable({
+  id: 'some-injectable',
+
+  instantiate: () => ({
+    someGeneralProperty: 'some string',
+    someSpecificProperty: 42,
+  }),
+
+  injectionToken: someTokenWithGeneralProperty,
+});
+
+// given injection token and implementation which is more specific than the token, when injected as injectable, typing is specific
+expectType<{ someGeneralProperty: string; someSpecificProperty: number }>(
+  di.inject(someInjectableWithAlsoSpecificProperty),
+);
+
+// given injection token and implementation which is more specific than the token, when injected using injection token, typing is not specific
+expectType<{ someGeneralProperty: string }>(
+  di.inject(someTokenWithGeneralProperty),
+);
+
+// given injection token and implementation which is more specific than the token, when injecting many, typing is not specific
+expectType<{ someGeneralProperty: string }[]>(
+  di.injectMany(someTokenWithGeneralProperty),
+);
+
+const someInjectableForOverrides = getInjectable({
+  id: 'some-injectable',
+  instantiate: () => ({ someProperty: 42 }),
+});
+
+// given injectable, when overriding with matching instantiate, typing is OK
+expectType<void>(
+  di.override(someInjectableForOverrides, () => ({ someProperty: 84 })),
+);
+
+// given injectable, when overriding with not matching instantiate, typing is not OK
+expectError(
+  di.override(someInjectableForOverrides, () => ({
+    someProperty: 'some-not-number',
+  })),
+);
+
+// given injectable, when overriding with a more specific matching instantiate, typing is OK
+expectType<void>(
+  di.override(someInjectableForOverrides, () => ({
+    someProperty: 84,
+    someSpecificProperty: 42,
+  })),
+);
