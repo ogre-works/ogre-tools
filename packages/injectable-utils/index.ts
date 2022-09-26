05f48b459e6e3a89b670d45b26875bafe9fbc5bf
Iku-turso <mikko.aspiala@gmail.com>
2022-09-26 14:56:01 +0300

    fix: Export missing types from Runnable

--- packages/injectable-utils/index.ts
+++ packages/injectable-utils/index.ts
@@ -1,4 +1,6 @@
-import { runManyFor } from './src/runnable/run-many-for';
-import { runManySyncFor } from './src/runnable/run-many-sync-for';
+import { runManyFor, Runnable } from './src/runnable/run-many-for';
+import { runManySyncFor, RunnableSync } from './src/runnable/run-many-sync-for';
+
+export type { RunnableSync, Runnable };
 
 export { runManyFor, runManySyncFor };
