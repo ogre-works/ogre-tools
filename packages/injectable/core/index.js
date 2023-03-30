279875a3e857e47b5048368fe9c214fb20cabc84
Iku-turso <mikko.aspiala@gmail.com>
2023-03-30 08:18:56 +0300

    chore: Extract createInstantiationTargetDecorator to a file

--- packages/injectable/core/index.js
+++ packages/injectable/core/index.js
@@ -8,13 +8,15 @@ import createContainer from './src/dependency-injection-container/createContaine
 import isInjectable from './src/getInjectable/isInjectable';
 import isInjectionToken from './src/getInjectionToken/isInjectionToken';
 
+export { createInstantiationTargetDecorator } from './src/dependency-injection-container/createInstantiationTargetDecorator';
+
 export {
   deregistrationCallbackToken,
   injectionDecoratorToken,
-  createInstantiationTargetDecorator,
   instantiationDecoratorToken,
   registrationCallbackToken,
 } from './src/dependency-injection-container/tokens';
+
 export {
   createContainer,
   getInjectable,
