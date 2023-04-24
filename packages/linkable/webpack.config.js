410a78e11ed2d46886a61376afdbb0d960c8560a
Iku-turso <mikko.aspiala@gmail.com>
2023-04-24 13:25:52 +0300

    feat: Make built linkable runnable as npm bin script

--- packages/linkable/webpack.config.js
+++ packages/linkable/webpack.config.js
@@ -1,4 +1,10 @@
+const webpackConfig =
+  require('@ogre-tools/infrastructure-webpack-for-ts').webpackConfig;
+
+const ShebangPlugin = require('webpack-shebang-plugin');
+
 module.exports = {
-  ...require('@ogre-tools/infrastructure-webpack-for-ts').webpackConfig,
+  ...webpackConfig,
   performance: undefined,
+  plugins: [...(webpackConfig.plugins || []), new ShebangPlugin()],
 };
