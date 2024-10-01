06eec7dc5566865746ef41c06023d0a4959989ed
Iku-turso <mikko.aspiala@gmail.com>
2024-10-01 11:47:21 +0300

    chore: Add unit-tests to document how withInjectables works when non-dependency props change
    Co-authored-by: Janne Savolainen <janne.savolainen@live.fi>
    Signed-off-by: Iku-turso <mikko.aspiala@gmail.com>

--- packages/injectable/react/src/withInjectables/withInjectables.test.jsx
+++ packages/injectable/react/src/withInjectables/withInjectables.test.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { setImmediate as flushMicroTasks } from 'timers';
-import { render } from '@testing-library/react';
-import { act } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 
 import {
   createContainer,
@@ -430,6 +429,160 @@ describe('withInjectables', () => {
         expect(
           rendered.queryByTestId('some-placeholder'),
         ).not.toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('given nested components, with async dependencies, when rendered', () => {
+    let rendered;
+    let someProp;
+    let asyncDependencyMock;
+    let mountOfDecoratedComponentMock;
+    let createPlaceholderMock;
+
+    beforeEach(async () => {
+      mountOfDecoratedComponentMock = jest.fn();
+
+      const ToBeDecoratedComponent = ({ someDependency, ...props }) => {
+        React.useEffect(() => {
+          mountOfDecoratedComponentMock();
+        }, []);
+
+        return (
+          <div
+            data-testid="some-dumb-test-component"
+            data-some-dependency-test={someDependency}
+            {...props}
+          >
+            Some content: "{someDependency}"
+          </div>
+        );
+      };
+
+      asyncDependencyMock = asyncFn();
+      createPlaceholderMock = jest.fn();
+      const SmartChildTestComponent = withInjectables(ToBeDecoratedComponent, {
+        getProps: async (di, props) => ({
+          someDependency: await asyncDependencyMock(),
+          ...props,
+        }),
+
+        getPlaceholder: () => {
+          createPlaceholderMock();
+
+          return <div data-testid="some-placeholder" />;
+        },
+      });
+
+      const DumbParentTestComponent = observer(({ someProp }) => (
+        <div data-some-prop-test={someProp.get()}>
+          <SmartChildTestComponent />
+        </div>
+      ));
+
+      someProp = observable.box('some-initial-value');
+      rendered = mount(<DumbParentTestComponent someProp={someProp} />);
+    });
+
+    it('renders as placeholder', () => {
+      expect(rendered.baseElement).toMatchSnapshot();
+    });
+
+    it('does not render component yet', () => {
+      expect(
+        rendered.queryByTestId('some-dumb-test-component'),
+      ).not.toBeInTheDocument();
+    });
+
+    it('does not mount the decorated component yet', () => {
+      expect(mountOfDecoratedComponentMock).not.toHaveBeenCalled();
+    });
+
+    it('has placeholder', () => {
+      expect(rendered.queryByTestId('some-placeholder')).toBeInTheDocument();
+    });
+
+    describe('when the dependency resolves', () => {
+      beforeEach(async () => {
+        await act(async () => {
+          await asyncDependencyMock.resolve('some-async-value');
+        });
+      });
+
+      it('renders', () => {
+        expect(rendered.baseElement).toMatchSnapshot();
+      });
+
+      it('has component with async content', () => {
+        expect(rendered.baseElement).toHaveTextContent(
+          'Some content: "some-async-value"',
+        );
+      });
+
+      it('mounts the decorated component', () => {
+        expect(mountOfDecoratedComponentMock).toHaveBeenCalled();
+      });
+
+      it('has component', () => {
+        expect(
+          rendered.queryByTestId('some-dumb-test-component'),
+        ).toBeInTheDocument();
+      });
+
+      it('no longer has placeholder', () => {
+        expect(
+          rendered.queryByTestId('some-placeholder'),
+        ).not.toBeInTheDocument();
+      });
+
+      describe('when a (non-dependency) prop changes', () => {
+        beforeEach(async () => {
+          asyncDependencyMock.mockClear();
+          mountOfDecoratedComponentMock.mockClear();
+          createPlaceholderMock.mockClear();
+
+          await act(async () => {
+            runInAction(() => {
+              someProp.set('some-new-value');
+            });
+          });
+        });
+
+        it('renders the new value', () => {
+          expect(
+            rendered.baseElement.querySelector(
+              '[data-some-prop-test=some-new-value]',
+            ),
+          ).toBeInTheDocument();
+        });
+
+        it('does not instantiate async dependencies again', () => {
+          expect(asyncDependencyMock).not.toHaveBeenCalled();
+        });
+
+        it('does not create another placeholder', () => {
+          expect(createPlaceholderMock).not.toHaveBeenCalled();
+        });
+
+        it('does not mount the decorated component again', () => {
+          expect(mountOfDecoratedComponentMock).not.toHaveBeenCalled();
+        });
+
+        it('renders', () => {
+          expect(rendered.baseElement).toMatchSnapshot();
+        });
+
+        it('still has component with the async content', () => {
+          expect(rendered.baseElement).toHaveTextContent(
+            'Some content: "some-async-value"',
+          );
+        });
+
+        it('has no placeholder', () => {
+          expect(
+            rendered.queryByTestId('some-placeholder'),
+          ).not.toBeInTheDocument();
+        });
       });
     });
   });
